6fea698c21afabc31ebe49f314a3d88a78dbdecc
Viacheslav Mayorov <mayorovvn@gmail.com>
2019-04-19 17:47:24 +0300

    show not persisted readings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,30 +3,44 @@ class Reading < ApplicationRecord
 
   before_create :ensure_id
 
-  CACHE_KEY_ID = "#{Rails.env}_reading_id"
+  CACHE_KEY_LAST_ID = "#{Rails.env}_reading_id"
+  CACHE_KEY_ID = "#{Rails.env}_reading_"
 
   def self.setup_redis
-    REDIS.set CACHE_KEY_ID, Reading.maximum(:id).to_i, nx: true
+    REDIS.set CACHE_KEY_LAST_ID, Reading.maximum(:id).to_i, nx: true
   end
 
   def self.reset_redis
-    REDIS.del CACHE_KEY_ID
+    REDIS.del CACHE_KEY_LAST_ID
     setup_redis
   end
 
   def self.next_id
-    REDIS.incr CACHE_KEY_ID
+    REDIS.incr CACHE_KEY_LAST_ID
   end
 
   def self.create_later!(options={})
-    opts = { id: next_id }.merge(options)
+    opts = options.to_h.symbolize_keys
+    opts[:id] = next_id unless opts[:id]
 
-    delay.create! opts.symbolize_keys
+    delay.create! opts
+
+    REDIS.set (CACHE_KEY_ID + opts[:id].to_s), opts.to_json
 
     new opts
   end
 
   def ensure_id
     self.id ||= self.class.next_id
+  end
+
+  def self.find(id)
+    cached_value = REDIS.get(CACHE_KEY_ID + id.to_s)
+    if cached_value
+      cached_opts = JSON.parse(cached_value)
+      return new(cached_opts)
+    end
+
+    super
   end
 end
--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Reading, type: :model do
+  let(:thermostat) { create :thermostat }
+  let(:reading) { build :reading, thermostat: thermostat }
+
   it { should belong_to(:thermostat) }
+
+  describe '::create_later!' do
+    let!(:cached_reading) { Reading.create_later! reading.attributes }
+
+    it 'schedules the reading creation and stores the data in the cache' do
+      expect(Reading.method :create!).to be_delayed(cached_reading.attributes.symbolize_keys)
+      expect(REDIS.get "#{Rails.env}_reading_#{cached_reading.id}").
+        to eq cached_reading.attributes.symbolize_keys.to_json
+      expect { Sidekiq::Worker.drain_all }.to change { Reading.count }.by 1
+    end
+  end
+
+  describe '::find' do
+    it 'loads data from database' do
+      reading.save!
+
+      expect(Reading.find reading.id).to eq reading
+    end
+
+    context 'given not persisted data' do
+      let!(:cached_reading) { Reading.create_later! reading.attributes }
+
+      it 'fetches data from the cache' do
+        expect(Reading.find cached_reading.id).to eq cached_reading
+      end
+    end
+
+    context 'given wrong id' do
+      specify do
+        expect { Reading.find -1 }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
